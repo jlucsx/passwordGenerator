528fd6228c504adeb2b053f506ade1cca21101a2
rhiontk34 <jalucas.jall@gmail.com>
2021-05-21 22:11:17 -0300

    function to switch character's case
    this function switch the case of each character inside a given list - our quote text

--- password_gen.py
+++ password_gen.py
@@ -22,3 +22,14 @@ def remove_duplicates(quote):
 		if char.isalpha():
 			sanitized_qt.append(char)
 	return sanitized_qt
+
+
+def switchcase_char(uniq_chars_qt):
+	"switch the case of each character of a given unique-char-quote then return the result"
+	sanit_qt_switchcased = []
+	for i, char in enumerate(uniq_chars_qt):
+		if i % 2 == 0:
+			sanit_qt_switchcased.append(char.upper())
+			continue
+		sanit_qt_switchcased.append(char)
+	return sanit_qt_switchcased
